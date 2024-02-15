44f363aa405f87cc59017461ffbf20fbdef055f8
bouwazradhia <bouwazradhia@gmail.com>
2024-02-15 11:20:47 +0100

    test

--- Components/UserIItems.js
+++ Components/UserIItems.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { Ionicons } from "@expo/vector-icons";
 const UserIItems = ({ name }) => {
   return (
     <View style={styles.item}>
       <Text style={styles.userTXT}>{name}</Text>
+      <Ionicons name="person-remove-outline" size={24} color="black" />
     </View>
   );
 };
@@ -17,5 +18,8 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     padding: 8,
     borderRadius: 8,
+    flexDirection: "row",
+    justifyContent:
   },
+  userTXT: {},
 });
